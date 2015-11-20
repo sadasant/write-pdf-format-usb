f64cdfee516eeb60a88947a962e936410ad21da7
Daniel Rodríguez <sadasant@gmail.com>
2015-11-19 22:47:52 -0500

    better browser exec code

--- browser/browser.go
+++ browser/browser.go
@@ -1,12 +1,32 @@
 package browser
 
 import (
+	"fmt"
 	"os/exec"
+	"runtime"
 )
 
 var Verbose bool
 
 func Open(address string) {
 	logf("Opening chrome on mode app at %s", address)
-	exec.Command("google-chrome", "--app=http://"+address, "--app-shell-host-windows-bounds", "--window-position=300,50").CombinedOutput()
+
+	// The GET param no-cache is just a placeholder but the browser
+	// trully stops caching this with that
+	address = fmt.Sprintf("http://%s?no-cache=true", address)
+
+	binary := "google-chrome"
+	if runtime.GOOS == "windows" {
+		binary = "chrome.exe"
+	}
+
+	params := []string{
+		binary,
+		"-app=" + address,
+		"--window-size=300,400",
+		"--window-position=300,50",
+		"--disable-cache",
+	}
+
+	exec.Command("google-chrome", params...).CombinedOutput()
 }
