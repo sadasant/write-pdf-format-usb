89995805db378b5cafd0ce13255a29f7d8aeb983
Daniel Rodríguez <sadasant@gmail.com>
2015-12-12 23:17:18 -0500

    Controller Drives implementation for windows

--- server/Controllers.go
+++ server/Controllers.go
@@ -4,10 +4,18 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"os/exec"
+	"regexp"
+	"strings"
 )
 
 func ControllerDrives(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "A,B,C,D,E")
+	out, _ := exec.Command("wmic", "logicaldisk", "where", "drivetype=2", "get", "deviceid").CombinedOutput()
+	rege := regexp.MustCompile("[A-Z]:")
+	drives := rege.FindAllString(string(out), -1)
+	fmt.Println(drives)
+	// Format: "A,B,C,D,E"
+	fmt.Fprintf(w, strings.Join(drives, ","))
 }
 
 func ControllerKeepAlive(w http.ResponseWriter, r *http.Request) {
