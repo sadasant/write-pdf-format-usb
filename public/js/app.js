077220d0f2bf98b0ba62913af73500f61648fd6b
Daniel Rodríguez <sadasant@gmail.com>
2015-12-14 18:00:49 -0500

    changes to the first form

--- public/js/app.js
+++ public/js/app.js
@@ -26,7 +26,7 @@
         console.log("LOADING", name)
         $.ajax({
             method:  "GET",
-            url:     "/views/"+name+".html",
+            url:     "/views/"+name+".html?no-cache=true", // no-cache is just to avoid browser cache
             success: function(data) {
                 $content.html(data);
             }
@@ -36,10 +36,24 @@
     loadView($content.attr("view"));
     $content.attr("view", "");
 
+    // Move to next view
     $(window).on("click", function(e) {
         var $target = $(e.target);
         var view    = $target.attr("view");
         console.log($target, view);
         if (view) loadView(view);
     });
+
+    // Show content based on selects
+	$content.on("change", "select", function(e){
+		var $this = $(this);
+		var show  = $this.attr("show");
+		var value = $this.val();
+		console.log("Select changed", show, value);
+		if (show) {
+			if (show[0] === "#") $("[id^="+show.substr(1)+"]").addClass("hidden");
+			if (show[0] === ".") $("[class^="+show.substr(1)+"]").addClass("hidden");
+			$(show+"-"+value).removeClass("hidden");
+		}
+	});
 })();
